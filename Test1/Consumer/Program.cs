22f9237b89912d9bf6b01e42ddd4e1538c2ba20c
00004740 <azat.tekce@opet.com.tr>
2023-11-20 15:20:54 +0300

    first commit

---
+++ Test1/Consumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("topicexchange", type: ExchangeType.Topic);
+
+                string queueName = channel.QueueDeclare().QueueName;
+                string routingKey = ".Tegmen";
+
+                routingKey = args[0] switch
+                {
+                    "1" => $"*.*.Tegmen",
+                    "2" => $"*.#.Yuzbasi",
+                    "3" => $"#.Binbasi.#",
+                    "4" => $"Asker.Subay.Tegmen",
+                };
+
+                channel.QueueBind(queue: queueName, exchange: "topicexchange", routingKey: routingKey);
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume(queueName, false, consumer);
+                consumer.Received += (sender, e) =>
+                {
+                    Console.WriteLine($"{routingKey} {Encoding.UTF8.GetString(e.Body)} görevi aldı.");
+                    channel.BasicAck(e.DeliveryTag, false);
+                };
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ Test1/Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("topicexchange", type: ExchangeType.Topic);
+                for (int i = 1; i <= 100; i++)
+                {
+                    byte[] bytemessage = Encoding.UTF8.GetBytes($"{i}. görev verildi.");
+
+                    IBasicProperties properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+                    channel.BasicPublish(exchange: "topicexchange", routingKey: $"Asker.Subay.{(i % 2 == 0 ? "Yuzbasi" : (i % 11 == 0 ? "Binbasi" : "Tegmen"))}", basicProperties: properties, body: bytemessage);
+                }
+            }
+        }
+    }
+}
